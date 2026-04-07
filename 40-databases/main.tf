5cc482a3c77c74ed839ea89f88b4478edb17d130
srini <srinivaskaipu2705@gmail.com>
2026-04-07 00:43:56 -0400

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,30 +1,30 @@
-resource "aws_instance" "mongodb" {
+resource "aws_instance" "redis" {
   ami           = local.ami_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [local.mongodb_sg_id]
+  vpc_security_group_ids = [local.redis_sg_id]
   subnet_id = local.database_subnet_ids
 
   tags = merge (
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-mongodb" # roboshop-dev-mongodb
+        Name = "${local.common_name_suffix}-redis" # roboshop-dev-redis
     }
   )
 }
 
-resource "terraform_data" "mongodb" {
+resource "terraform_data" "redis" {
     triggers_replace = [
-        aws_instance.mongodb.id,
+        aws_instance.redis.id,
     ]
 
     connection {
         type = "ssh"
         user = "ec2-user"
         password = "DevOps321"
-        host = aws_instance.mongodb.private_ip
+        host = aws_instance.redis.private_ip
     }
 
-    # terraform copies this file to mongodb instance and then executes it
+    # terraform copies this file to redis instance and then executes it
     provisioner "file" {
       source = "bootstrap.sh"
       destination = "/tmp/bootstrap.sh"
@@ -33,7 +33,49 @@ resource "terraform_data" "mongodb" {
     provisioner "remote-exec" {
       inline = [ 
         "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh"
+        #"sudo sh /tmp/bootstrap.sh"
+        "sudo sh /tmp/bootstrap.sh mongodb"
+       ]
+    }
+  
+}
+
+resource "aws_instance" "redis" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.redis_sg_id]
+  subnet_id = local.database_subnet_ids
+
+  tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-redis" # roboshop-dev-redis
+    }
+  )
+}
+
+resource "terraform_data" "redis" {
+    triggers_replace = [
+        aws_instance.redis.id,
+    ]
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.redis.private_ip
+    }
+
+    # terraform copies this file to redis instance and then executes it
+    provisioner "file" {
+      source = "bootstrap.sh"
+      destination = "/tmp/bootstrap.sh"
+    }
+
+    provisioner "remote-exec" {
+      inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh redis"
        ]
     }
   
