7f7e31e6b0b82ea6b4971dff4c742a39c2c0cd56
srini <srinivaskaipu2705@gmail.com>
2026-04-07 00:52:41 -0400

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -1,30 +1,30 @@
-resource "aws_instance" "redis" {
+resource "aws_instance" "mongodb" {
   ami           = local.ami_id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [local.redis_sg_id]
+  vpc_security_group_ids = [local.mongodb_sg_id]
   subnet_id = local.database_subnet_ids
 
   tags = merge (
     local.common_tags,
     {
-        Name = "${local.common_name_suffix}-redis" # roboshop-dev-redis
+        Name = "${local.common_name_suffix}-mongodb" # roboshop-dev-mongodb
     }
   )
 }
 
-resource "terraform_data" "redis" {
+resource "terraform_data" "mongodb" {
     triggers_replace = [
-        aws_instance.redis.id,
+        aws_instance.mongodb.id,
     ]
 
     connection {
         type = "ssh"
         user = "ec2-user"
         password = "DevOps321"
-        host = aws_instance.redis.private_ip
+        host = aws_instance.mongodb.private_ip
     }
 
-    # terraform copies this file to redis instance and then executes it
+    # terraform copies this file to mongodb instance and then executes it
     provisioner "file" {
       source = "bootstrap.sh"
       destination = "/tmp/bootstrap.sh"
