57aaabe3a834858e4af94d48e69d54a560058350
srini <srinivaskaipu2705@gmail.com>
2026-04-07 01:02:20 -0400

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -80,3 +80,85 @@ resource "terraform_data" "redis" {
     }
   
 }
+
+resource "aws_instance" "rabbitmq" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
+  subnet_id = local.database_subnet_ids
+
+  tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-rabbitmq" # roboshop-dev-rabbitmq
+    }
+  )
+}
+
+resource "terraform_data" "rabbitmq" {
+    triggers_replace = [
+        aws_instance.rabbitmq.id,
+    ]
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.rabbitmq.private_ip
+    }
+
+    # terraform copies this file to rabbitmq instance and then executes it
+    provisioner "file" {
+      source = "bootstrap.sh"
+      destination = "/tmp/bootstrap.sh"
+    }
+
+    provisioner "remote-exec" {
+      inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh rabbitmq"
+       ]
+    }
+  
+}
+
+resource "aws_instance" "mysql" {
+  ami           = local.ami_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [local.mysql_sg_id]
+  subnet_id = local.database_subnet_ids
+
+  tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-mysql" # roboshop-dev-mysql     
+    }
+  )
+}
+
+resource "terraform_data" "mysql" {
+    triggers_replace = [
+        aws_instance.mysql.id,
+    ]
+
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.mysql.private_ip
+    }
+
+    # terraform copies this file to mysql instance and then executes it
+    provisioner "file" {
+      source = "bootstrap.sh"
+      destination = "/tmp/bootstrap.sh"
+    }
+
+    provisioner "remote-exec" {
+      inline = [ 
+        "chmod +x /tmp/bootstrap.sh",
+        "sudo sh /tmp/bootstrap.sh mysql"
+       ]
+    }
+  
+}
