e6454df9aa6cc1f9415094e4b02c9ce939f2bb51
srini <srinivaskaipu2705@gmail.com>
2026-04-13 19:44:46 -0400

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -23,6 +23,6 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
 
-data "aws_ssm_parameter" "catalogue_sg_ids" {
-  name = "/${var.project_name}/${var.environment}/catalogue_sg_ids"
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
 }
