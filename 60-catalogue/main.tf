fd2311db036b69dd2c6d3f38984d1e24c48cfdb4
srini <srinivaskaipu2705@gmail.com>
2026-04-13 21:38:02 -0400

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -41,3 +41,17 @@ resource "terraform_data" "catalogue" {
        ]
     }
 }
+
+#stop the instance to take AMI image
+resource "aws_ec2_instance_state" "catalogue" {
+    instance_id = aws_instance.catalogue.id
+    state = "stopped"
+    depends_on = [ terraform_data.catalogue ]
+}
+
+resource "aws_ami_from_instance" "catalogue" {
+    name = "${local.common_name_suffix}-catalogue-ami" # roboshop-dev-catalogue-ami
+     description = "AMI for catalogue service"
+    source_instance_id = aws_instance.catalogue.id
+    depends_on = [ aws_ec2_instance_state.catalogue ]
+}
