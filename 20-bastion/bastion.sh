c1638c70655be514136c7ee63c55d75dc67699c1
srini <srinivaskaipu2705@gmail.com>
2026-04-14 20:25:13 -0400

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# growing the /home volume to 50 GB as we will be installing terraform and ansible in the bastion host and also running ansible playbooks from the bastion host which may require more space for logs and other files. The default volume size is 8 GB which is not sufficient for our needs.
+growpart /dev/nvme0n1 4
+lvextend -L +30G /dev/mapper/RootVG-homeVol
+xfs_groefs /home
+
 # This script is used to bootstrap the databases module of the roboshop-dev-infra Terraform code. It is executed by Terraform as a provisioner after the database instances are created.
  sudo yum install -y yum-utils
  sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
  sudo yum -y install terraform
- 
--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -61,3 +61,119 @@ resource "aws_ami_from_instance" "catalogue" {
         }
     )
 }
+
+#target group for catalogue service
+resource "aws_lb_target_group" "catalogue" {
+    name = "${local.common_name_suffix}-catalogue-tg" # roboshop-dev-catalogue-tg
+    port = 8080
+    protocol = "HTTP"
+    vpc_id = local.vpc_id
+    deregistration_delay = 60 #waiting period before deleting the target from the target group, this is required to ensure that the existing connections are not dropped immediately when the target is deleted from the target group, this is especially important for stateful applications like catalogue service which may have long running connections.
+    
+    health_check {
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+        interval = 10
+        timeout = 2
+        path = "/health"
+        protocol = "HTTP"
+        matcher = "200-299"
+    }
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-catalogue-tg" # roboshop-dev-catalogue-tg
+        }
+    )
+} 
+
+# aws launch template for catalogue service
+resource "aws_launch_template" "catalogue" {
+    name = "${local.common_name_suffix}-catalogue-lt" # roboshop-dev-catalogue-lt
+    image_id = aws_ami_from_instance.catalogue.id
+    instance_initiated_shutdown_behavior = "terminate"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [local.catalogue_sg_id]
+    # key_name = "roboshop-dev-key" # replace with your key pair name
+    # user_data = file("catalogue.sh")
+
+
+    #tags attached to the instance created by the launch template
+    tag_specifications {
+      resource_type = "instance"
+        tags = merge (
+          local.common_tags,
+          {
+              Name = "${local.common_name_suffix}-catalogue" # roboshop-dev-catalogue
+          }
+      )
+    }
+
+    # tags attached to the volume created by instance
+     tag_specifications {
+       resource_type = "volume"     
+     tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-catalogue-lt" # roboshop-dev-catalogue-lt
+        }
+      )
+    }
+
+    # tags attached to the launch template
+    tags = merge (
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-catalogue-lt" # roboshop-dev-catalogue-lt
+        }
+    )
+}
+
+# auto scaling group for catalogue service
+resource "aws_autoscaling_group" "catalogue" {
+    name = "${local.common_name_suffix}-catalogue-asg" # roboshop-dev-catalogue-asg
+    max_size = 10
+    min_size = 1
+    health_check_grace_period = 100
+    health_check_type = "ELB"
+    desired_capacity = 1
+    force_delete = false 
+    launch_template {
+        id = aws_launch_template.catalogue.id
+        version = aws_launch_template.catalogue.latest_version
+    }
+    vpc_zone_identifier = [local.private_subnet_ids]
+    target_group_arns = [aws_lb_target_group.catalogue.arn]
+    dynamic "tag" { #we will get the iterator with name as value
+      for_each = merge(
+        local.common_tags,
+        {
+            Name = "${local.common_name_suffix}-catalogue" # roboshop-dev-catalogue
+        }
+      )
+      content {
+        key = tag.key
+        value = tag.value
+        propagate_at_launch = true
+      }
+    }
+
+    timeouts {
+      delete = "15m"
+    }
+}
+
+#auto scaling policy for catalogue service
+resource "aws_autoscaling_policy" "catalogue" {
+    name = "${local.common_name_suffix}-catalogue-cpu-policy" # roboshop-dev-catalogue-cpu-policy
+    autoscaling_group_name = aws_autoscaling_group.catalogue.name
+    policy_type = "TargetTrackingScaling"
+    target_tracking_configuration {
+        predefined_metric_specification {
+            predefined_metric_type = "ASGAverageCPUUtilization"
+        }
+        target_value = 75.0
+    }
+}
+
+#listener rule to forward traffic to catalogue target group
