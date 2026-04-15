29d39a43a70789dbd03b8915ca398cea1d55be5c
srini <srinivaskaipu2705@gmail.com>
2026-04-14 21:00:50 -0400

    terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -3,9 +3,11 @@ resource "aws_alb" "backend_alb" {
   internal           = true
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
+  #subnets should be private subnets as this is internal ALB and we don't want to expose it to the internet
   subnets            = local.private_subnet_ids
+  
+  enable_deletion_protection = false # we can enable this in production environment to prevent accidental deletion of ALB
 
-  # enable_deletion_protection = true # to prevent accidental deletion of ALB
 
   tags = merge(
     local.common_tags,
@@ -28,5 +30,19 @@ resource "aws_alb_listener" "backend_alb_listener" {
       message_body = "Hi , I am from backend ALB HTTP listener"
       status_code  = "200"
     }
+  }
+}
+
+# creating a record in route53 to point to our backend ALB
+resource "aws_route53_record" "backend_alb" {
+  zone_id = var.zone_id
+  name = "*.backend-alb-${var.environment}.${var.domain_name}" # *.backend-alb-{environment}.roboshop.com
+  type = "A"
+
+  alias {
+    # this are ALB details,not our domain details, we are pointing our domain to ALB DNS name and zone id
+    name = aws_elb.backend_alb.dns_name # ALB DNS name
+    zone_id = aws_elb.backend_alb.zone_id
+    evaluate_target_health = true
   }
 }
--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+  default = "Z1003128U2OCSI7JICC9"
+}
+
+variable "domain_name" {
+  default = "srini.store"
+}
