469a73abcecf1e24c16742c67094bc5563b19052
srini <srinivaskaipu2705@gmail.com>
2026-04-21 19:39:12 -0400

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -14,6 +14,7 @@ xfs_growfs /home
 # creating databases
 cd /home/ec2-user
 git clone https://github.com/srinivaskaipu2705-alt/roboshop-dev-infra.git
+chown ec2-user:ec2-user -R roboshop-dev-infra
 cd roboshop-dev-infra/40-databases
 terraform init
 terraform apply -auto-approve
--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,5 +1,12 @@
+# module "components" {
+#   source = "../../terraform-roboshop-component"
+#   component = var.component
+#   rule_priority = var.rule_priority
+# }
+
 module "components" {
-  source = "../terraform-roboshop-component"
-  component = var.component
-  rule_priority = var.rule_priority
+    for_each = var.components
+    source = "git::https://github.com/srinivaskaipu2705-alt/terraform-roboshop-component.git?ref=main"
+    component = each.key
+    rule_priority = var.value.rule_priority
 }
